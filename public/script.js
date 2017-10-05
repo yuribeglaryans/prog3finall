4aa200240bf3ff5100a745890f5a019ab46549f7
user <user@lab1523.tumo.lab>
2017-10-05 20:04:35 +0400

    final project

---
+++ public/script.js
@@ -0,0 +1,114 @@
+google.charts.load('45', { packages: ['corechart', 'table',] });
+
+google.charts.setOnLoadCallback(drawScatterChart);
+google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawTable);
+
+function drawScatterChart() {
+    $.ajax({
+        url: "/articles",
+        dataType: "json",
+        success: function (jsonData) {
+            var data = new google.visualization.DataTable();
+            data.addColumn('date', 'date');
+            data.addColumn('number', 'views');
+
+
+            for (var i = 0; i < jsonData.length; i++) {
+                data.addRow([
+                    new Date(jsonData[i].date),
+                    jsonData[i].cost,
+
+                ]);
+
+            }
+
+            var options = {
+                legend: 'left',
+                title: 'According to salary surveies',
+                is3D: false,
+                width: '100%',
+                height: '100%',
+                hAxis: { title: 'Month' },
+                vAxis: { title: 'Salary', minValue: 0, maxValue: 2500},
+            };
+            //console.log(data.toJSON());
+            // Instantiate and draw the chart.
+            var chart = new google.visualization.ScatterChart(document.getElementById('chart_div0'));
+            chart.draw(data, options);
+        }
+    });
+};
+
+function drawChart() {
+    $.ajax({
+        url: "/articles",
+        dataType: "json",
+        success: function (jsonData) {
+            var data = new google.visualization.DataTable();
+            data.addColumn('number', 'cost');
+            for (var i = 0; i < jsonData.length; i++) {
+                data.addRow([
+                    jsonData[i].cost,
+                ]);
+
+            }
+
+            var options = {
+                title: 'Works',
+                colors: ['orange'],
+                legend: { position: 'none' },
+                hAxis: { title: 'Work in salary' },
+                vAxis: { title: 'Count of vacancy' },
+                histogram: {
+                    bucketSize: 100000,
+                    maxNumBuckets: 1000000
+                }
+            };
+            //console.log(data.toJSON());
+            // Instantiate and draw the chart.
+            var chart = new google.visualization.Histogram(document.getElementById('chart_div1'));
+            chart.draw(data, options);
+        }
+    });
+};
+
+
+function drawTable() {
+    $.ajax({
+        url: "/articles",
+        dataType: "json",
+        success: function (jsonData) {
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Work');
+            data.addColumn('number', 'Salary');
+            data.addColumn('date', 'Date');
+
+            for (var i = 0; i < jsonData.length; i++) {
+                data.addRow([
+                    jsonData[i].work,
+                    jsonData[i].cost,
+                    new Date(jsonData[i].date)
+                ]);
+            }
+
+            var options = {
+                allowHtml: true,
+                showRowNumber: true,
+                width: '100%',
+                height: '100%'
+            };
+
+            var table = new google.visualization.Table(document.getElementById('barformat_div'));
+            var formatter = new google.visualization.BarFormat({ width: 100 });
+            formatter.format(data, 2); // Apply formatter to 3rd column
+            table.draw(data, options);
+        }
+    });
+}
+
+$(window).resize(function () {
+    drawScatterChart();
+    drawChart();
+    drawTable();
+});
---
+++ scraper.js
@@ -0,0 +1,93 @@
+const scrapeIt = require("scrape-it");
+var jsonfile = require("jsonfile");
+
+var page_number = 1;
+var x = {
+    articles: []
+};
+var url;
+var file = 'data_1.json'
+
+function sc(page_number) {
+    url = "http://ashxatanqner.com/?ad_cat=%D5%A1%D5%BC%D5%A1%D5%BB%D5%A1%D6%80%D5%AF%D5%B8%D6%82%D5%B4-%D5%A5%D5%B4&paged=" + page_number;
+    scrapeIt(url, {
+
+        articles: {
+            listItem: ".post-block",
+            data: {
+                work: {
+                    selector: "div.post-right.full > h3 > a"
+
+                },
+                cost: {
+                    selector: "div.post-right.full > div.tags.price-wrap > span",
+                    convert: function(x) {
+                        var y = x.split(" ")[0]
+                        var z = y.replace(/,/i, '');
+                        var a = parseInt(z)
+                        return a;
+                    }
+                },
+                name: "div.post-right.full > p.post-meta > span.dashicons-before.owner",
+                data: "div.post-right.full > p.post-meta > span.dashicons-before.clock > span",
+                informations: "div.post-right.full > p.post-desc",
+                views: "div.post-right.full > p.stats",
+                image_url: {
+                    selector: "div.post-left > a > img",
+                    attr: "src"
+                },
+                url: {
+                    selector: "div.post-left > a",
+                    attr: "href"
+                },
+                date: {
+                    selector: "div.post-left > a > img",
+                    attr: "src",
+                    convert: function(x){
+                        var re = /uploads\/(\d*\/\d*)\//i;
+                        if (re.test(x)) {
+                          var found = x.match(re);
+                          var c = found[1].split("/")[0]
+                          var u = found[1].split("/")[1]
+                          var k = parseInt(c)
+                          var m = parseInt(u)
+                          var arr=[];
+                          arr.push(k,m      )
+                          return arr;
+                        } else {
+                          return "";
+                        }
+                    }
+                }
+                
+            }
+
+        }
+
+
+
+
+    }, (err, page) => {
+        x.articles.push(...page.articles);
+        console.log('x: ' + x.articles.length);
+        console.log('url: ' + url);
+        if (page_number == 5) { //88
+            jsonfile.writeFile(file, x, {
+                spaces: 2
+            }, function(err) {
+                console.error(err)
+            });
+        } else {
+            setTimeout(
+                () => {
+                    page_number++;
+                    console.log(page_number);
+                    sc(page_number);
+                }, 3000);
+        }
+
+
+    });
+    
+}
+sc(page_number);
